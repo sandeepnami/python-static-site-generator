0398f2b48fabc054467e136d9a743f80a8bb5a71
sandeepnami <nami.sandeep@gmail.com>
2022-02-18 18:57:31 +0530

    ssg.py

--- ssg/site.py
+++ ssg/site.py
@@ -3,12 +3,12 @@ import ssg.parsers
 
 class Site:
     def __init__(self, source, dest, parsers=None):
-        self._source = Path(source)
-        self._dest = Path(dest)
-        self._parsers = parsers or []  # when parsers=None instance variable _parsers is assigned to empty list
+        self.source = Path(source)
+        self.dest = Path(dest)
+        self.parsers = parsers or []  # when parsers=None instance variable _parsers is assigned to empty list
 
     def create_dir(self,path):
-        directory = self._dest / path.relative_to(self._source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
     def load_parsers(self, extension):
@@ -17,15 +17,17 @@ class Site:
                 return parser
                 
     def run_parser(self, path):
-        self.load_parsers(path.suffix)
-        if parser != None:
-            ResourceParser.parse()
+        parser = self.load_parsers(path.suffix)
+        if parser is not None:   # comparision with None
+            ssg.parsers.parse(path, self.source, self.dest)
+        else:
+            print('Not Implemented')
             
     def build(self):
-        self._dest.mkdir(parents=True, exist_ok=True)
-        for path in self._source.rglob('*'):   # recursive glob
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob('*'):   # recursive glob
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                self.run_parser()
+                self.run_parser(path)
     
