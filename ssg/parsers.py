0ee0355abed02738cb89f96834d84d6f084b1848
sandeepnami <nami.sandeep@gmail.com>
2022-02-21 13:55:54 +0530

    ssg.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -54,8 +54,8 @@ class ReStructuredText(Parser):
     extensions = [".rst"]
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = publish_parts(content.body, writer_name='html5')
-        self.write(path, dest, html['html_body'])
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
